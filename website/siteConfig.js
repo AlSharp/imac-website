47f16ecae36dbce1423e920458d2cdbac5a5e28d
Albert Sharapov <elistaphilly@gmail.com>
2018-12-27 23:09:36 -0500

    siteConfig.js edited

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'IMAC', // Title for your website.
   tagline: 'IntelLiDrives Multi Axes Motion and Automation Controller',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://AlSharp.github.io', // Your website URL
+  baseUrl: '/imac/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'imac-website',
+  organizationName: 'AlSharp',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
