7c1ff929b9a543c6aba67987372f3c04cbf1ea6c
AlSharp <elistaphilly@gmail.com>
2024-09-22 22:52:13 -0400

    remove files

--- website-v3/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/albert/projects/imac-website/website-v3/node_modules/infima/dist/css/default/default.css"),
-  require("/home/albert/projects/imac-website/website-v3/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/albert/projects/imac-website/website-v3/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/albert/projects/imac-website/website-v3/src/css/customTheme.css"),
-];
